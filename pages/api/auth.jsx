4ee6f20aa9220bc9470c98ba6a08a3798a9c17c0
Cesar Jaramillo <csarjaramillo89@yahoo.com>
2023-06-11 17:54:23 -0700

    added createUser function

--- pages/api/auth.jsx
+++ pages/api/auth.jsx
@@ -30,3 +30,25 @@ export default async function logout() {
     console.error('Error signing out:', error.message);
   }
 }
+
+export default async function createUser(email, password, first_name) {
+  try {
+    const { user, error } = await supabase.auth.signUp({
+      email: email,
+      password: password,
+    }, {
+      data: {
+        first_name: first_name,
+      },
+    });
+
+    if (error) {
+      console.log('Error creating user:', error.message);
+    } else {
+      console.log('User created successfully:', user);
+    }
+  } catch (error) {
+    console.error('Error creating user:', error.message);
+  }
+}
+
