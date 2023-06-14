35e283c238ae710bf1db2487c07adb654f211947
Cesar Jaramillo <csarjaramillo89@yahoo.com>
2023-06-13 17:05:59 -0700

    added Comments component

--- pages/posts/[id].tsx
+++ pages/posts/[id].tsx
@@ -2,6 +2,8 @@ import { useRouter } from 'next/router';
 import { useEffect, useState } from 'react';
 import { getPostById } from '../api/auth';
 
+import Comments from '../../components/Comments'
+
 type Post = {
   id: string;
   title: string;
@@ -21,20 +23,23 @@ const PostPage = () => {
     async function fetchPost() {
       try {
         const postData = await getPostById(id as string);
+  
         if ('id' in postData && 'title' in postData) {
           setPost(postData as Post);
         } else {
           console.error('Received invalid post data:', postData);
         }
+  
       } catch (error: any) {
         console.error('Error fetching post:', error.message);
       }
     }
-
+  
     if (id) {
       fetchPost();
     }
   }, [id]);
+  
 
   if (!post) {
     return <div>Loading...</div>;
@@ -48,6 +53,8 @@ const PostPage = () => {
       {post.paragraph_3 && <p>{post.paragraph_3}</p>}
       {post.paragraph_4 && <p>{post.paragraph_4}</p>}
       {post.paragraph_5 && <p>{post.paragraph_5}</p>}
+      <br />
+      <Comments postId={post.id} />
     </div>
   );
   
