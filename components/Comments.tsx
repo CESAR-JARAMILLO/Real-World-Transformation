1f4c2465385c0fb9bf1cd9d22534bf70db88c2c7
Cesar Jaramillo <csarjaramillo89@yahoo.com>
2023-06-13 23:36:22 -0700

    added CommentForm component

--- components/Comments.tsx
+++ components/Comments.tsx
@@ -1,6 +1,10 @@
 import { useEffect, useState } from 'react';
 import { getCommentsByPostId } from '../pages/api/auth';
+import { supabase } from '../lib/supabaseClient'
+import { Session } from '@supabase/supabase-js'
+
 import Comment from './Comment';
+import CommentForm from './CommentForm';
 
 interface Comment {
   id: string;
@@ -16,11 +20,24 @@ interface CommentsProps {
 
 const Comments: React.FC<CommentsProps> = ({ postId }) => {
   const [comments, setComments] = useState<Comment[] | null>(null);
+  const [session, setSession] = useState<Session | null>(null);
+
+  async function getCurrentSession() {
+    const { data, error } = await supabase.auth.getSession();
+  
+    if (error) {
+      console.error('Error getting session:', error.message);
+      throw error;
+    }
+  
+    return data ? data.session : null;
+  }
 
   useEffect(() => {
     async function fetchComments() {
       try {
         const commentsData = await getCommentsByPostId(postId);
+        const sessionData = await getCurrentSession();
 
         if (Array.isArray(commentsData) && commentsData.every(comment => {
           return 'id' in comment && 'comment' in comment && 'post_id' in comment && 'user_id' in comment && 'created_at' in comment;
@@ -29,6 +46,8 @@ const Comments: React.FC<CommentsProps> = ({ postId }) => {
         } else {
           console.error('Received invalid comments data:', commentsData);
         }
+
+        setSession(sessionData);
       } catch (error: any) {
         console.error('Error fetching comments:', error.message);
       }
@@ -49,6 +68,7 @@ const Comments: React.FC<CommentsProps> = ({ postId }) => {
       {comments.map((comment) => (
         <Comment key={comment.id} comment={comment} />
       ))}
+      {session && <CommentForm postId={postId} />}
     </div>
   );
 };
