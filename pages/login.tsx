05725b54e7eab243d8ebe29b145cd8900ae151c0
Cesar Jaramillo <csarjaramillo89@yahoo.com>
2023-06-22 14:52:57 -0700

    updated and styled using Chakra

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,6 +1,17 @@
 import { useState } from 'react';
-import { signIn, signOut } from './api/auth'
+import { signIn, signOut } from './api/auth';
 import { useRouter } from 'next/router';
+import {
+  Box,
+  Button,
+  Flex,
+  FormControl,
+  FormLabel,
+  Input,
+  Heading,
+  Center,
+  Link
+} from '@chakra-ui/react';
 
 const LoginPage = () => {
   const [password, setPassword] = useState('');
@@ -31,32 +42,31 @@ const LoginPage = () => {
   }
 
   return (
-    <>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label htmlFor="email">Email</label>
-          <input
-            type="email"
-            id="email"
-            value={email}
-            onChange={handleEmailChange}
-            autoComplete="username"
-          />
-        </div>
-        <div>
-          <label htmlFor="password">Password</label>
-          <input
-            type="password"
-            id="password"
-            value={password}
-            onChange={handlePasswordChange}
-            autoComplete="current-password"
-          />
-        </div>
-        <button type="submit">Submit</button>
-      </form>
-      <button onClick={handleSignOut}>Sign Out</button>
-    </>
+    <Flex height="100vh" alignItems="center" justifyContent="center">
+      <Box p={5} shadow="md" borderWidth={1} borderRadius="md" width="sm">
+        <Center marginBottom={5}>
+          <Heading size="lg">Login</Heading>
+        </Center>
+        <form onSubmit={handleSubmit}>
+          <FormControl id="email">
+            <FormLabel>Email address</FormLabel>
+            <Input type="email" value={email} onChange={handleEmailChange} autoComplete="username" />
+          </FormControl>
+
+          <FormControl id="password" mt={4}>
+            <FormLabel>Password</FormLabel>
+            <Input type="password" value={password} onChange={handlePasswordChange} autoComplete="current-password" />
+          </FormControl>
+
+          <Button type="submit" width="full" mt={4}>
+            Submit
+          </Button>
+        </form>
+        <Center marginTop={5}>
+          <Link href="/signup">Don't have an account?</Link>
+        </Center>
+      </Box>
+    </Flex>
   );
 };
 
