80d3987bcd4e0d945e2c28dcf33cec4b61a4a028
Cesar Jaramillo <csarjaramillo89@yahoo.com>
2023-06-22 15:03:58 -0700

    updated and styled using Chakra

--- pages/signup.tsx
+++ pages/signup.tsx
@@ -1,53 +1,98 @@
 import { useState } from 'react';
 import { supabase } from '../lib/supabaseClient';
-import { signUp } from './api/auth'
+import { signUp } from './api/auth';
+import {
+  Box,
+  Button,
+  Flex,
+  FormControl,
+  FormLabel,
+  Input,
+  Heading,
+  Center,
+  Link
+} from '@chakra-ui/react';
 
-const SignUp = () => {
+const SignUpPage = () => {
   const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState(''); // Added state for password confirmation
   const [email, setEmail] = useState('');
+  const [confirmEmail, setConfirmEmail] = useState(''); // Added state for email confirmation
 
   const handleEmailChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setEmail(e.target.value);
+  };
+
+  const handleConfirmEmailChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setConfirmEmail(e.target.value);
   };
 
   const handlePasswordChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setPassword(e.target.value);
   };
 
+  const handleConfirmPasswordChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setConfirmPassword(e.target.value);
+  };
+
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
 
-    signUp(email, password)  // Updated function call
+    if(email !== confirmEmail) {
+      alert('Emails do not match!');
+      return;
+    }
+
+    if(password !== confirmPassword) {
+      alert('Passwords do not match!');
+      return;
+    }
+
+    signUp(email, password)
 
     setEmail('');
     setPassword('');
+    setConfirmEmail(''); // Clear confirmation email
+    setConfirmPassword(''); // Clear confirmation password
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <div>
-        <label htmlFor="email">Email</label>
-        <input
-          type="email"
-          id="email"
-          value={email}
-          onChange={handleEmailChange}
-          autoComplete="username"
-        />
-      </div>
-      <div>
-        <label htmlFor="password">Password</label>
-        <input
-          type="password"
-          id="password"
-          value={password}
-          onChange={handlePasswordChange}
-          autoComplete="current-password"
-        />
-      </div>
-      <button type="submit">Submit</button>
-    </form>
-  )
-}
+    <Flex height="100vh" alignItems="center" justifyContent="center">
+      <Box p={5} shadow="md" borderWidth={1} borderRadius="md" width="sm">
+        <Center marginBottom={5}>
+          <Heading size="lg">Sign Up</Heading>
+        </Center>
+        <form onSubmit={handleSubmit}>
+          <FormControl id="email">
+            <FormLabel>Email address</FormLabel>
+            <Input type="email" value={email} onChange={handleEmailChange} autoComplete="username" />
+          </FormControl>
 
-export default SignUp
+          <FormControl id="confirm-email" mt={4}> {/* Added email confirmation */}
+            <FormLabel>Confirm Email address</FormLabel>
+            <Input type="email" value={confirmEmail} onChange={handleConfirmEmailChange} autoComplete="username" />
+          </FormControl>
+
+          <FormControl id="password" mt={4}>
+            <FormLabel>Password</FormLabel>
+            <Input type="password" value={password} onChange={handlePasswordChange} autoComplete="current-password" />
+          </FormControl>
+
+          <FormControl id="confirm-password" mt={4}> {/* Added password confirmation */}
+            <FormLabel>Confirm Password</FormLabel>
+            <Input type="password" value={confirmPassword} onChange={handleConfirmPasswordChange} autoComplete="current-password" />
+          </FormControl>
+
+          <Button type="submit" width="full" mt={4}>
+            Submit
+          </Button>
+        </form>
+        <Center marginTop={5}>
+          <Link href="/login">Already have an account?</Link>
+        </Center>
+      </Box>
+    </Flex>
+  );
+};
+
+export default SignUpPage;
