2d59c4dfe6ab55c6eb82c349d737ca32af290780
Cesar Jaramillo <csarjaramillo89@yahoo.com>
2023-06-22 10:21:05 -0700

    updated signup page

--- pages/signup.tsx
+++ pages/signup.tsx
@@ -1,17 +1,11 @@
 import { useState } from 'react';
-import { useUser } from '@supabase/auth-helpers-react';
 import { supabase } from '../lib/supabaseClient';
 import { signUp } from './api/auth'
 
 const SignUp = () => {
   const [password, setPassword] = useState('');
   const [email, setEmail] = useState('');
-  const [firstName, setFirstName] = useState('');
 
-  const handleNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setFirstName(e.target.value);
-  };
-  
   const handleEmailChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setEmail(e.target.value);
   };
@@ -23,7 +17,7 @@ const SignUp = () => {
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
 
-    signUp(email, password, firstName)
+    signUp(email, password)  // Updated function call
 
     setEmail('');
     setPassword('');
@@ -31,16 +25,6 @@ const SignUp = () => {
 
   return (
     <form onSubmit={handleSubmit}>
-      <div>
-        <label htmlFor="name">Name</label>
-        <input
-          type="name"
-          id="name"
-          value={firstName}
-          onChange={handleNameChange}
-          autoComplete="username"
-        />
-      </div>
       <div>
         <label htmlFor="email">Email</label>
         <input
