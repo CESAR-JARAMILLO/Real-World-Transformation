9fe9112a15a9d2cdca8a0348acb5c519d7ff2bdb
Cesar Jaramillo <csarjaramillo89@yahoo.com>
2023-06-26 13:53:35 -0700

    updated validation

--- pages/login.tsx
+++ pages/login.tsx
@@ -76,7 +76,7 @@ const LoginPage = () => {
               value={password} 
               onChange={handlePasswordChange} 
               autoComplete="current-password"
-              pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" 
+              // pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" 
               title="Must contain at least one number and one uppercase and lowercase letter, and at least 8 or more characters."
             />
           </FormControl>
