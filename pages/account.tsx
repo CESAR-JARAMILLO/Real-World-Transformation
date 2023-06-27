199461631948c2510c6eb56df1c606e0ba54798f
Cesar Jaramillo <csarjaramillo89@yahoo.com>
2023-06-27 16:00:21 -0700

    implimented UserAvatar and image upload

--- pages/account.tsx
+++ pages/account.tsx
@@ -1,39 +1,43 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, SetStateAction } from 'react';
 import { Box, Button, Flex, FormControl, FormLabel, Input, Heading, Center, VStack, Spinner, Text } from '@chakra-ui/react';
 import { updateUser, getCurrentUser, getCurrentUserProfile, signOut, deleteComments } from './api/auth';
 import { deleteUser } from './api/adminAuth';
 import { supabase } from '../lib/supabaseClient';
-import { Session } from '@supabase/supabase-js'
+import { Session } from '@supabase/supabase-js';
 import { useRouter } from 'next/router';
+
+import UserAvatar from '@/components/UserAvatar';
 
 const Account = () => {
   const [email, setEmail] = useState('');
   const [fullName, setFullName] = useState('');
   const [username, setUsername] = useState('');
   const [session, setSession] = useState<Session | null>(null);
-  const [loading, setLoading] = useState(true);
-  const router = useRouter()
+  const [avatarUrl, setAvatarUrl] = useState('');
+  const router = useRouter();
+  const user = session?.user;
 
   useEffect(() => {
     const fetchAndSetUserData = async () => {
       const user = await getCurrentUser();
       const userProfile = await getCurrentUserProfile();
-  
+
       if (user && user.email) {
         setEmail(user.email);
       }
-  
+
       if (userProfile && userProfile[0]) {
         setFullName(userProfile[0].full_name);
         setUsername(userProfile[0].username);
+        setAvatarUrl(userProfile[0].avatar_url);
       } else {
-        router.push('/login')
+        router.push('/login');
       }
     };
 
     async function getCurrentSession() {
       const { data, error } = await supabase.auth.getSession();
-    
+
       if (error) {
         console.error('Error getting session:', error.message);
         throw error;
@@ -44,17 +48,11 @@ const Account = () => {
       } else {
         setSession(null);
       }
-
-      setLoading(false);
     }
-  
+
     fetchAndSetUserData();
-
-    getCurrentSession()
-
+    getCurrentSession();
   }, [router]);
-  
-
 
   const handleEmailChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setEmail(e.target.value);
@@ -71,12 +69,12 @@ const Account = () => {
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
     try {
-      await updateUser(email, fullName, username);
+      await updateUser(email, fullName, username, avatarUrl);
       setEmail('');
       setFullName('');
       setUsername('');
       alert('Update successful!');
-      router.push('/posts')
+      router.push('/posts');
     } catch (error) {
       alert('Failed to update user info.');
     }
@@ -89,7 +87,7 @@ const Account = () => {
         await deleteComments(user?.id);
         await deleteUser(user?.id);
         alert('Account deleted successfully!');
-        signOut()
+        signOut();
         router.push('/login');
       } catch (error) {
         alert('Failed to delete account.');
@@ -107,12 +105,20 @@ const Account = () => {
   }
 
   return (
-    <Flex minHeight="100vh" width="full" align="center" justifyContent="center">
+    <Flex minHeight="100vh" width="full" align="center" justifyContent="center" direction="column">
       <Box borderWidth={1} px={4} width="full" maxWidth="500px" borderRadius={4} textAlign="center" boxShadow="lg">
         <Box p={4}>
-        <Center marginBottom={5}>
-          <Heading size="lg">Edit Profile</Heading>
-        </Center>
+          <Center marginBottom={5}>
+            <Heading size="lg">Edit Profile</Heading>
+          </Center>
+          <UserAvatar
+            uid={user?.id}
+            url={avatarUrl}
+            size={150}
+            onUpload={(url: SetStateAction<string>) => {
+              setAvatarUrl(url);
+            }}
+          />
           <Box as="form" onSubmit={handleSubmit}>
             <FormControl id="email" mb={4}>
               <FormLabel>Email</FormLabel>
