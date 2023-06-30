cb92258085a73f77017f0eb49d233898fe6db974
Cesar Jaramillo <csarjaramillo89@yahoo.com>
2023-06-30 09:40:17 -0700

    added Categories component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import Head from 'next/head';
 import Hero from '@/components/HomeComponents/Hero';
+import Categories from '@/components/HomeComponents/Categories';
 
 export default function Home() {
   return (
@@ -12,6 +13,7 @@ export default function Home() {
       </Head>
 
       <Hero />
+      <Categories />
     </>
   );
 }
