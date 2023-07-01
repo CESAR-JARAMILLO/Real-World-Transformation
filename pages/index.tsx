8198f220f0d827cbdc73470b003947d621c2ce89
Cesar Jaramillo <csarjaramillo89@yahoo.com>
2023-07-01 12:11:44 -0700

    added Description component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import Head from 'next/head';
 import Hero from '@/components/HomeComponents/Hero';
 import Categories from '@/components/HomeComponents/Categories';
+import Description from '@/components/HomeComponents/Description';
 
 export default function Home() {
   return (
@@ -14,6 +15,7 @@ export default function Home() {
 
       <Hero />
       <Categories />
+      <Description />
     </>
   );
 }
