fb62d50eff7b80c4167122b640cec44cd9359b5c
Cesar Jaramillo <csarjaramillo89@yahoo.com>
2023-07-03 13:31:56 -0700

    added textColor prop

--- components/ImageTextBox.tsx
+++ components/ImageTextBox.tsx
@@ -3,6 +3,7 @@ import { Box, Text, Heading, useMediaQuery, Flex, Image } from "@chakra-ui/react
 interface ImageTextBoxProps {
   imageUrl: any;
   backgroundColor: string;
+  textColor: string;
   header: string;
   bodyTextOne: string;
   bodyTextTwo: string | null;
@@ -12,6 +13,7 @@ interface ImageTextBoxProps {
 const ImageTextBox: React.FC<ImageTextBoxProps> = ({
   imageUrl,
   backgroundColor,
+  textColor,
   header,
   bodyTextOne,
   bodyTextTwo,
@@ -32,7 +34,7 @@ const ImageTextBox: React.FC<ImageTextBoxProps> = ({
           w={{ base: "100%", sm: "90%", md: "88%", lg: "85%" }}
           overflow="hidden"
         >
-          <Box w="60%" h="100%" color="white" flex="1" display="flex" flexDirection="column" justifyContent="center" px={20}>
+          <Box w="60%" h="100%" color={textColor} flex="1" display="flex" flexDirection="column" justifyContent="center" px={20}>
             <Heading mb={8}>{header}</Heading>
             <Text mb={6}>{bodyTextOne}</Text>
             <Text>{bodyTextTwo}</Text>
@@ -52,7 +54,7 @@ const ImageTextBox: React.FC<ImageTextBoxProps> = ({
           w={{ base: "100%", sm: "90%" }}
           overflow="hidden"
         >
-          <Box w="100%" color="white" flex="1" display="flex" flexDirection="column" justifyContent="center" px={{ base: 6, sm: 12 }} py={16}>
+          <Box w="100%" color={textColor} flex="1" display="flex" flexDirection="column" justifyContent="center" px={{ base: 6, sm: 12 }} py={16}>
             <Heading mb={4}>{header}</Heading>
             <Text mb={3}>{bodyTextOne}</Text>
             <Text>{bodyTextTwo}</Text>
