c2fc3deb1f3f164e5ba608a6415f58c2b89db5dc
Cesar Jaramillo <csarjaramillo89@yahoo.com>
2023-07-04 10:22:34 -0700

    created SearchPosts component

---
+++ components/SearchPosts.tsx
@@ -0,0 +1,49 @@
+import React, { useState, useEffect } from 'react';
+import { Button, Flex, Select } from "@chakra-ui/react";
+import { supabase } from '../lib/supabaseClient';
+
+interface SearchPostsProps {
+  onSearch: (category: string) => void;
+}
+
+export const SearchPosts: React.FC<SearchPostsProps> = ({ onSearch }) => {
+  const [categories, setCategories] = useState<string[]>([]);
+  const [selectedCategory, setSelectedCategory] = useState('');
+
+  useEffect(() => {
+    fetchCategories();
+  }, []);
+
+  const fetchCategories = async () => {
+    const { data, error } = await supabase.from('posts').select('category');
+
+    if (error) {
+      console.error('Error fetching categories:', error.message);
+    }
+
+    if (Array.isArray(data)) {
+      const uniqueCategories = Array.from(new Set(data.map(item => item.category)));
+      setCategories(uniqueCategories);
+    }
+  };
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    onSearch(selectedCategory);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <Flex mt={20} direction="row">
+        <Select placeholder="Select category" value={selectedCategory} onChange={(e) => setSelectedCategory(e.target.value)}>
+          {categories.map((category) => (
+            <option key={category} value={category}>
+              {category.charAt(0).toUpperCase()+ category.slice(1)}
+            </option>
+          ))}
+        </Select>
+        <Button colorScheme="twitter" p={5} ml={5} type="submit">Search</Button>
+      </Flex>
+    </form>
+  );
+};
