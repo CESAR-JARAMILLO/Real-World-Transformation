ad11679ef81eb7d89b17f929cfa1de22ad46ff64
Cesar Jaramillo <csarjaramillo89@yahoo.com>
2023-09-14 16:47:41 -0700

    added useEffect

--- components/WorkoutLog.tsx
+++ components/WorkoutLog.tsx
@@ -1,6 +1,12 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 const WorkoutLog = () => {
+
+  useEffect(() => {
+    // fetch data from api
+    // set data to state
+  }, [])
+
   return (
     <div>WorkoutLog</div>
   )
