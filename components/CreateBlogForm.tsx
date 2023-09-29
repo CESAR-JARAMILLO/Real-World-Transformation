e2405f3245045bdbaa86ae1d6f6b50e90198bc28
Cesar Jaramillo <csarjaramillo89@yahoo.com>
2023-09-28 23:17:07 -0700

    updates

--- components/CreateBlogForm.tsx
+++ components/CreateBlogForm.tsx
@@ -1,38 +1,43 @@
 import React, { useState } from 'react';
 import {
-  Box,
   Button,
   Card,
   CardBody,
-  FormControl,
-  FormLabel,
-  Input,
   Stack,
   StackDivider,
-  Text,
-  Textarea,
 } from '@chakra-ui/react';
 import { useSupabaseClient, useUser } from '@supabase/auth-helpers-react';
 import FormSection from './FormSection';
 import FormHeader from './FormHeader';
 import FormPageSlug from './FormPageSlug';
+import { useRouter } from 'next/router';
 
 const CreateBlogForm = () => {
   const user = useUser();
   const supabase = useSupabaseClient();
+  const router = useRouter();
   const [blogTitle, setBlogTitle] = useState<string>('');
   const [paragraphOneTitle, setParagraphOneTitle] = useState<string>('');
   const [paragraphOne, setParagraphOne] = useState<string>('');
   const [slug, setSlug] = useState<string>('');
   const [mainImageUrl, setMainImageUrl] = useState<string | null>(null);
   const [sectionOneImageUrl, setSectionOneImageUrl] = useState<string | null>(null);
+  const [sectionTwoImageUrl, setSectionTwoImageUrl] = useState<string | null>(null);
 
   const handleBlogTitleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setBlogTitle(e.target.value);
   }
 
+  const handleParagraphTwoTitleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setParagraphOneTitle(e.target.value);
+  }
+
   const handleParagraphOneTitleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setParagraphOneTitle(e.target.value);
+  }
+
+  const handleParagraphTwoChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setParagraphOne(e.target.value);
   }
 
   const handleParagraphOneChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
@@ -49,14 +54,20 @@ const CreateBlogForm = () => {
       title: blogTitle,
       main_image_url: mainImageUrl,
       subtitle_1: paragraphOneTitle,
+      subtitle_2: paragraphOneTitle,
       sub_image_url_1: sectionOneImageUrl,
+      sub_image_url_2: sectionTwoImageUrl,
       paragraph_1: paragraphOne,
+      paragraph_2: paragraphOne,
       category: 'health',
       user_id: user?.id,
       slug,
 
     })
-    .then(response => {console.log(response)})
+    .then(response => {
+      console.log(response)
+      router.push(`/blogs/${slug}`)
+    })
   }
 
   return (
@@ -72,6 +83,12 @@ const CreateBlogForm = () => {
             handleParagraphOneTitleChange={handleParagraphOneTitleChange}
             handleParagraphOneChange={handleParagraphOneChange}
             setSectionOneImageUrl={setSectionOneImageUrl}
+          />
+          <FormSection
+            sectionTitle={'Section 2'}
+            handleParagraphOneTitleChange={handleParagraphTwoTitleChange}
+            handleParagraphOneChange={handleParagraphTwoChange}
+            setSectionOneImageUrl={setSectionTwoImageUrl}
           />
           <FormPageSlug
             handleSlugChange={handleSlugChange}
